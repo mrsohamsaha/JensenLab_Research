8c69b7cbab7b5f91ec24170ee9161880396bfd19
mrsohamsaha <mr.soham.saha@gmail.com>
2019-07-31 18:50:26 -0500

    Added mixing to Ryan's Experiment

--- Opentrons/Ryans_Exp_Multi.py
+++ Opentrons/Ryans_Exp_Multi.py
@@ -1,11 +1,11 @@
 from opentrons import labware, instruments
 
-tiprack = labware.load('opentrons-tiprack-300ul', '10')
-tiprack1 = labware.load('opentrons-tiprack-300ul', '7')
-tiprack2 = labware.load('opentrons-tiprack-300ul', '11')
-plate_1 = labware.load('96-flat', '2')
-plate_10 = labware.load('96-flat', '5')
-plate_100 = labware.load('96-flat', '8')
+tiprack = labware.load('opentrons_96_tiprack_300ul', '10')
+tiprack1 = labware.load('opentrons_96_tiprack_300ul', '7')
+tiprack2 = labware.load('opentrons_96_tiprack_300ul', '11')
+plate_1 = labware.load('corning_96_wellplate_360ul_flat', '2')
+plate_10 = labware.load('corning_96_wellplate_360ul_flat', '5')
+plate_100 = labware.load('corning_96_wellplate_360ul_flat', '8')
 
 plate_agar='agar_plate_9_9_9_10'
 if plate_agar not in labware.list():
@@ -21,7 +21,7 @@ agar_plate_1 = labware.load(plate_agar, '3')
 agar_plate_10 = labware.load(plate_agar, '4')
 agar_plate_100 = labware.load(plate_agar, '9')
 
-dummy = labware.load('96-deep-well', '1')
+dummy = labware.load('usascientific_96_wellplate_2.4ml_deep', '1')
 
 pipette = instruments.P50_Multi(
 	mount = 'left',
@@ -30,81 +30,81 @@ pipette = instruments.P50_Multi(
 dil_vol = 10
 spot_vol = 7
 
-for i in range(1,12):
-	pipette.pick_up_tip()
-
-	pipette.aspirate(spot_vol, dummy.columns(str(i)))
-	pipette.aspirate(spot_vol, plate_1.columns(str(i)))
-	pipette.dispense(2*spot_vol, agar_plate_1.columns(str(i)))
-
-	pipette.aspirate(dil_vol, dummy.columns(str(i)))
-	pipette.aspirate(dil_vol, plate_1.columns(str(i)))
-	pipette.dispense(2*dil_vol, plate_10.columns(str(i)))
-
-	pipette.drop_tip()
-	pipette.pick_up_tip()
-
-	pipette.aspirate(spot_vol, dummy.columns(str(i)))
-	pipette.aspirate(spot_vol, plate_10.columns(str(i)))
-	pipette.dispense(2*spot_vol, agar_plate_10.columns(str(i)))
-
-	pipette.aspirate(dil_vol, dummy.columns(str(i)))
-	pipette.aspirate(dil_vol, plate_10.columns(str(i)))
-	pipette.dispense(2*dil_vol, plate_100.columns(str(i)))
-
-	pipette.drop_tip()
-	pipette.pick_up_tip()
-
-	pipette.aspirate(spot_vol, dummy.columns(str(i)))
-	pipette.aspirate(spot_vol, plate_100.columns(str(i)))
-	pipette.dispense(2*spot_vol, agar_plate_100.columns(str(i)))
-
-	pipette.drop_tip()
-	
 # for i in range(1,13):
-# 	print(plate_1.columns(str(i)))
-	# pipette.pick_up_tip()
+# 	pipette.pick_up_tip()
 
-	# # pipette.aspirate(spot_vol, dummy.columns(str(i)))
-	# pipette.consolidate(
-	# spot_vol,
-	# plate_1.columns(str(i)),
-	# agar_plate_1.columns(str(i)),
-	# blow_out=True,
-	# new_tip='never')
+# 	pipette.aspirate(spot_vol, dummy.columns(str(i)))
+# 	pipette.aspirate(spot_vol, plate_1.columns(str(i)))
+# 	pipette.dispense(2*spot_vol, agar_plate_1.columns(str(i)))
 
-	# pipette.aspirate(dil_vol, dummy.columns(str(i)))
-	# pipette.transfer(
-	# dil_vol,
-	# plate_1.columns(str(i)),
-	# plate_10.columns(str(i)),
-	# blow_out=True,
-	# mix_after=(5,20))
+# 	pipette.aspirate(dil_vol, dummy.columns(str(i)))
+# 	pipette.aspirate(dil_vol, plate_1.columns(str(i)))
+# 	pipette.dispense(2*dil_vol, plate_10.columns(str(i)))
 
-	# pipette.pick_up_tip()
+# 	pipette.drop_tip()
+# 	pipette.pick_up_tip()
 
-	# pipette.aspirate(spot_vol, dummy.columns(str(i)))
-	# pipette.transfer(
-	# spot_vol,
-	# plate_10.columns(str(i)),
-	# agar_plate_10.columns(str(i)),
-	# blow_out=True,
-	# new_tip='never')
+# 	pipette.aspirate(spot_vol, dummy.columns(str(i)))
+# 	pipette.aspirate(spot_vol, plate_10.columns(str(i)))
+# 	pipette.dispense(2*spot_vol, agar_plate_10.columns(str(i)))
 
-	# pipette.aspirate(dil_vol, dummy.columns(str(i)))
-	# pipette.transfer(
-	# dil_vol,
-	# plate_10.columns(str(i)),
-	# plate_100.columns(str(i)),
-	# blow_out=True,
-	# mix_after=(5,20))
+# 	pipette.aspirate(dil_vol, dummy.columns(str(i)))
+# 	pipette.aspirate(dil_vol, plate_10.columns(str(i)))
+# 	pipette.dispense(2*dil_vol, plate_100.columns(str(i)))
 
-	# pipette.pick_up_tip()
+# 	pipette.drop_tip()
+# 	pipette.pick_up_tip()
+
+# 	pipette.aspirate(spot_vol, dummy.columns(str(i)))
+# 	pipette.aspirate(spot_vol, plate_100.columns(str(i)))
+# 	pipette.dispense(2*spot_vol, agar_plate_100.columns(str(i)))
+
+# 	pipette.drop_tip()
+	
+for i in range(1,2):
+	# print(plate_1.columns(str(i)))
+	pipette.pick_up_tip()
 
 	# pipette.aspirate(spot_vol, dummy.columns(str(i)))
-	# pipette.transfer(
-	# spot_vol,
-	# plate_100.columns(str(i)),
-	# agar_plate_100.columns(str(i)),
-	# blow_out=True)
+	pipette.consolidate(
+	spot_vol,
+	[dummy.columns(str(i)), plate_1.columns(str(i))],
+	agar_plate_1.columns(str(i)),
+	blow_out=True,
+	new_tip='never')
+
+	# pipette.aspirate(dil_vol, dummy.columns(str(i)))
+	pipette.consolidate(
+	dil_vol,
+	[dummy.columns(str(i)), plate_1.columns(str(i))],
+	plate_10.columns(str(i)),
+	blow_out=True,
+	mix_after=(5,20))
+
+	pipette.pick_up_tip()
+
+	# pipette.aspirate(spot_vol, dummy.columns(str(i)))
+	pipette.consolidate(
+	spot_vol,
+	[dummy.columns(str(i)), plate_10.columns(str(i))],
+	agar_plate_10.columns(str(i)),
+	blow_out=True,
+	new_tip='never')
+
+	# pipette.aspirate(dil_vol, dummy.columns(str(i)))
+	pipette.consolidate(
+	dil_vol,
+	[dummy.columns(str(i)), plate_10.columns(str(i))],
+	plate_100.columns(str(i)),
+	blow_out=True,
+	mix_after=(5,20))
+
+	pipette.pick_up_tip()
+
+	# pipette.aspirate(spot_vol, dummy.columns(str(i)))
+	pipette.consolidate(
+	spot_vol,
+	[dummy.columns(str(i)), plate_100.columns(str(i))],
+	agar_plate_100.columns(str(i)),
+	blow_out=True)
 
