6c10cb3fca282f2f992007dc2e77bde5b48a760e
mrsohamsaha <mr.soham.saha@gmail.com>
2019-08-17 15:05:52 -0500

    Started Upload Image Process

--- ColonyPicker/ColonyPicker/pickColony/views.py
+++ ColonyPicker/ColonyPicker/pickColony/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
+	if request.method == 'POST':
+		uploaded_file = request.FILES['image']
+		print(uploaded_file.name)
+		print(uploaded_file.size)
 	return render(request, 'home.html')
