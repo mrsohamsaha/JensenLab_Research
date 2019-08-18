9317aea2424e0c760f61f0018b87f0eca806bc81
Soham Saha <sohams2@illinois.edu>
2019-08-18 16:33:05 -0500

    Started file storage for images

--- ColonyPicker/ColonyPicker/ColonyPicker/urls.py
+++ ColonyPicker/ColonyPicker/ColonyPicker/urls.py
@@ -17,7 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from pickColony import views
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
 	path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('threshold', views.threshold, name='threshold')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ColonyPicker/ColonyPicker/pickColony/views.py
+++ ColonyPicker/ColonyPicker/pickColony/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from base64 import b64encode
+
+from django.core.files.storage import FileSystemStorage
 
 def home(request):
 	if request.method == 'POST':
 		uploaded_file = request.FILES['image']
-		print(uploaded_file.name)
-		print(uploaded_file.size)
+		fs = FileSystemStorage()
+		name = fs.save(uploaded_file.name, uploaded_file)
+		url = fs.url(name)
+		return render(request, 'threshold.html', {'url': url})
 	return render(request, 'home.html')
+
+def threshold(request):
+	return render(request, 'threshold.html')
